deb6e13dac3ca6fabc1553c46fd8e2c3e6078c61
Priyanshi-Chauhan <priyanshichauhanx06@gmail.com>
2023-09-05 00:21:17 +0530

    updated name

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -2,7 +2,7 @@ import React from 'react'
 import './Header.css';
 
 const Header = () => {
-  return <span onClick={()=>window.scroll(0,0)} className="header">🎬 Entertainment Hub 🎥</span>;
+  return <span onClick={()=>window.scroll(0,0)} className="header">🎬 FILM HUNT 🎥</span>;
 }
 
 export default Header
